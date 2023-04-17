955560ca9fe6958e22a193897b4b431c3a1e84a7
cmcaquilala <cmcaquilala@up.edu.ph>
2023-04-17 10:03:56 +0800

    added ALLOWED_HOSTS

--- forecastingprototype/settings.py
+++ forecastingprototype/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2(+e%8ae2mt&%hmjajwq^nc&f)=qob8$@f##*jzfaita6h!is_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
